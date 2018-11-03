34e4f9f289e328d0f6fe355a6695e446a59236a4
Samuel Ward <wards4@aston.ac.uk>
2018-11-03 17:36:17 +0000

    added collision for map layer with collision tag

--- GDX/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ GDX/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -12,6 +12,7 @@ public class DesktopLauncher
 		config.width = 528; //172
 		config.height = 768; //256
 		config.title = "Team 10: Hack";
+		config.foregroundFPS = 60;
 		new LwjglApplication(new PrisonEscapeGame(), config);
 	}
 }
