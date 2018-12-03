57fb3b5dc5e75148199e0a483f85cf90ddbf03b2
Hamza Zaman <36890317+zamanmh1@users.noreply.github.com>
2018-12-03 23:01:40 +0000

    Revert "Raw Hud"
    This reverts commit cb0de8b192bd344da9711c39395a962a17be2b83.

--- GDX/core/src/com/mygdx/prisonescapegame/screens/MapScreen.java
+++ GDX/core/src/com/mygdx/prisonescapegame/screens/MapScreen.java
@@ -27,7 +27,6 @@ import com.mygdx.game.io.InteractionController;
 import com.mygdx.game.io.PlayerMovementController;
 import com.mygdx.game.model.TiledModel;
 import com.mygdx.game.tween.SpriteAccessor;
-import com.mygdx.prisonescape.scenes.Hud;
 import com.mygdx.prisonescapegame.GameHandler;
 import com.mygdx.prisonescapegame.GameSettings;
 import com.mygdx.prisonescapegame.PrisonEscapeGame;
@@ -47,22 +46,22 @@ import aurelienribon.tweenengine.TweenManager;
  * 
  */
 
-public class MapScreen implements Screen {
-	private TiledMap tilemap;
+public class MapScreen implements Screen
+{	
+	private TiledMap tilemap; 
 	private OrthogonalTiledMapRenderer mapRenderer;
 	private OrthographicCamera oCamera;
-	private Viewport gamePort;
 	private Actor player;
-
+	
 	private ArrayList<Item> items; // Items to be drawn on each rendered frame
-
+	
 	private InputMultiplexer inputHandler; // Allows for multiple user inputs to be used
 	private PlayerMovementController movementHandler;
 	private InteractionController interactionHandler;
-
+	
 	private boolean isPaused;
-
-	private TiledModel model;
+	
+	private TiledModel model;	
 
 	private Sprite optionBackground;
 	private Sprite playButtonMenuInActive;
@@ -86,8 +85,8 @@ public class MapScreen implements Screen {
 	private static final int VOLUME_BUTTON_HEIGHT = 50;
 	private Sprite volumeButtonMuted;
 	private Sprite volumeButtonFull;
+
 	
-	private Hud hud;
 
 	public MapScreen(Actor player, PrisonEscapeGame game) {
 
@@ -96,12 +95,12 @@ public class MapScreen implements Screen {
 		tween = new TweenManager();
 		mapRenderer = new OrthogonalTiledMapRenderer(null);
 		tilemap = null;
-
-		batch = new SpriteBatch();
+		
+		batch = new SpriteBatch();	
 
 		movementHandler = new PlayerMovementController(player);
 		interactionHandler = new InteractionController(player);
-
+		
 		inputHandler = new InputMultiplexer();
 		inputHandler.addProcessor(movementHandler);
 		inputHandler.addProcessor(interactionHandler);
@@ -116,66 +115,64 @@ public class MapScreen implements Screen {
 		checkPlayButtonMouseOver = false;
 		checkExitButtonMouseOver = false;
 		
-		
-
 	}
-
-	public void setMap(String map, GameHandler gameHandler, int newX, int newY) {
-
+	
+	public void setMap(String map, GameHandler gameHandler, int newX, int newY) {	
+		
 		tilemap = new TmxMapLoader().load(map);
-		/**
-		 * If already a map being shown, try to dispose of it. However, if it is the
-		 * first map to be shown there is nothing to dispose of.
+		/** 
+		 * If already a map being shown, try to dispose of it.
+		 * However, if it is the first map to be shown there is nothing to dispose of. 
 		 */
 		try {
-
+			
 			mapRenderer.getMap().dispose();
-
-		} catch (NullPointerException e) {
+			
+			
+			
+		} catch  (NullPointerException e) {	
 			// Initial call to method to setup first map.
 			// Maybe output a message to welcome player to game?
 		}
 
 		mapRenderer.setMap(tilemap);
-
+		
 		model = new TiledModel(tilemap);
 		getTiledModel().getTile(newX, newY).setActor(player);
-
-		interactionHandler.setItemHandler(gameHandler); // high coupling (bad) provides way for interaction controller
-														// to handle finding items
-
+		
+		interactionHandler.setItemHandler(gameHandler); // high coupling (bad) provides way for interaction controller to handle finding items
+		
 		items = new ArrayList<Item>(); // Resets items in map
-
+		
+		
 	}
+	
+	
 
 	// Needed for rendering items in map
 	public void addItemToMap(Item i) {
 		items.add(i);
 	}
-
+	
 	// Stop given item being rendered in map
 	public void removeItemFromMap(Item i) {
 		items.remove(i);
 	}
-
+	
 	@Override
-	public void show() {
-		// Gdx.graphics.setWindowedMode(528, 768);
-		// mapRenderer = new OrthogonalTiledMapRenderer(tilemap); //initialises the
-		// Orthogonal (top-down) renderer for the map
-		oCamera = new OrthographicCamera(); // creates a camera to display the map on screen
-		// oCamera.setToOrtho(false, 11,16);
+	public void show() 
+	{		
+		//Gdx.graphics.setWindowedMode(528, 768);
+		//mapRenderer = new OrthogonalTiledMapRenderer(tilemap); //initialises the Orthogonal (top-down) renderer for the map
+		oCamera = new OrthographicCamera(); //creates a camera to display the map on screen
+		//oCamera.setToOrtho(false, 11,16);
+	
+		oCamera.setToOrtho(false, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
+		//Sets the camera and renders the scene from the bottom left. /3 to zoom in to match the size of the window.
 		
-		gamePort = new FitViewport(PrisonEscapeGame.WIDTH, PrisonEscapeGame.HEIGHT, oCamera);
-		hud = new Hud(game.getGameController().getSpriteBatch());
-		
-		oCamera.setToOrtho(false, Gdx.graphics.getWidth() / 3, Gdx.graphics.getHeight() / 3);
-		// Sets the camera and renders the scene from the bottom left. /3 to zoom in to
-		// match the size of the window.
-
 		Gdx.input.setInputProcessor(inputHandler);
 		Tween.registerAccessor(Sprite.class, new SpriteAccessor());
-
+		
 		Tween.set(optionBackground, SpriteAccessor.ALPHA).target(0f).start(tween);
 		Tween.to(optionBackground, SpriteAccessor.ALPHA, 1.0f).target(0.9f).start(tween);
 		Tween.set(volumeButtonMuted, SpriteAccessor.ALPHA).target(0f).start(tween);
@@ -190,66 +187,74 @@ public class MapScreen implements Screen {
 		Tween.to(exitButtonMenuActive, SpriteAccessor.ALPHA, 1.0f).target(1f).start(tween);
 		Tween.set(exitButtonMenuInActive, SpriteAccessor.ALPHA).target(0f).start(tween);
 		Tween.to(exitButtonMenuInActive, SpriteAccessor.ALPHA, 1.0f).target(1f).start(tween);
-
+		
 	}
 
 	@Override
-	public void render(float delta) {
-		// float oldDelta = delta;
-		// if (menuPressed) {
-		// delta = 0;
-		// }
-
+	public void render(float delta) 
+	{	
+		//float oldDelta = delta;
+		//if (menuPressed) {
+		//	delta = 0;
+		//}
+		
 		// updates using time since last render call
 		movementHandler.update(delta);
 		player.update(delta);
 		tween.update(delta);
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+		
 		// Smooth camera based on updated player position
-		oCamera.position.set(player.getWorldX() + 0.5f, player.getWorldY() + 0.5f, 0);
-
+		oCamera.position.set(player.getWorldX()+0.5f, player.getWorldY()+0.5f, 0);
+		
 		oCamera.update();
-
+		
 		mapRenderer.setView(oCamera);
 		mapRenderer.render();
-		// renders the map and sets the view of the camera to display the map
-
+		//renders the map and sets the view of the camera to display the map
+		
 		mapRenderer.getBatch().begin();
-		// player.draw(mapRenderer.getBatch());
-
-		mapRenderer.getBatch().draw(player.getSprite(), player.getWorldX(), player.getWorldY(), GameSettings.TILE_SIZE,
+		//player.draw(mapRenderer.getBatch());
+		
+		
+		mapRenderer.getBatch().draw(player.getSprite(),
+				player.getWorldX(), 
+				player.getWorldY(),
+				GameSettings.TILE_SIZE,
 				GameSettings.TILE_SIZE); // Render player
-
-		// Rendering the items in the given map
-		for (Item i : items) {
+		
+		//Rendering the items in the given map
+		for(Item i : items)
+		{
 			i.getSprite().setPosition(i.getWorldX(), i.getWorldY()); // Testing if restartItems() was being called.
 			i.getSprite().draw(mapRenderer.getBatch());
-		}
-
-		// delta = oldDelta;
-
+		}		
+		
+		//delta = oldDelta;
+		
 		mapRenderer.getBatch().end();
 		batch.begin();
-
+		
 		if (menuKeyCheck() == true) {
-
+			
+			
+			
 			optionBackground.setPosition(Gdx.graphics.getWidth() / 2 - optionBackground.getWidth() / 2,
 					Gdx.graphics.getHeight() / 2 - optionBackground.getHeight() / 2);
-
+			
+			
+			
 			optionBackground.draw(batch);
 			playButtonMenu();
 			exitButtonMenu();
 			volumeButton();
-
+			
 		}
 		batch.end();
 		
-		game.getGameController().getSpriteBatch().setProjectionMatrix(hud.stage.getCamera().combined);
-		hud.stage.draw();
-
-	}
+		}
+		
 
 	private boolean menuKeyCheck() {
 		if (Gdx.input.isKeyJustPressed(Keys.ESCAPE)) {
@@ -262,16 +267,15 @@ public class MapScreen implements Screen {
 
 		}
 		return menuPressed;
-
+		
 	}
-
 	private void playButtonMenu() {
 		int x = (int) (Gdx.graphics.getWidth() / 2 - playButtonMenuInActive.getWidth() / 2);
 		if (Gdx.input.getX() < x + PLAY_BUTTON_WIDTH && Gdx.input.getX() > x
 				&& PrisonEscapeGame.HEIGHT - Gdx.input.getY() < PLAY_BUTTON_Y + PLAY_BUTTON_HEIGHT
 				&& PrisonEscapeGame.HEIGHT - Gdx.input.getY() > PLAY_BUTTON_Y) {
 
-			playButtonMenuActive.setPosition(x, PLAY_BUTTON_Y);
+			playButtonMenuActive.setPosition(x,PLAY_BUTTON_Y);
 			playButtonMenuActive.setSize(PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
 			playButtonMenuActive.draw(batch);
 
@@ -283,22 +287,24 @@ public class MapScreen implements Screen {
 				checkPlayButtonMouseOver = true;
 
 			}
+			
 
 			if (Gdx.input.isTouched()) {
 
 				menuPressed = false;
+				
 
 			}
 		} else {
 			checkPlayButtonMouseOver = false;
-			playButtonMenuInActive.setPosition(x, PLAY_BUTTON_Y);
+			playButtonMenuInActive.setPosition(x,PLAY_BUTTON_Y);
 			playButtonMenuInActive.setSize(PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
 			playButtonMenuInActive.draw(batch);
 
 		}
-
+		
 	}
-
+	
 	private void exitButtonMenu() {
 		int x = (int) (Gdx.graphics.getWidth() / 2 - exitButtonMenuInActive.getWidth() / 2);
 		if (Gdx.input.getX() < x + EXIT_BUTTON_WIDTH && Gdx.input.getX() > x
@@ -317,20 +323,20 @@ public class MapScreen implements Screen {
 				checkExitButtonMouseOver = true;
 
 			}
+			
 
 			if (Gdx.input.justTouched()) {
 				Tween.registerAccessor(Sprite.class, new SpriteAccessor());
 
 				Tween.set(exitButtonMenuActive, SpriteAccessor.ALPHA).target(0).start(tween);
-				Tween.to(exitButtonMenuActive, SpriteAccessor.ALPHA, 0.5f).target(1).repeatYoyo(0, 0)
-						.setCallback(new TweenCallback() {
+				Tween.to(exitButtonMenuActive, SpriteAccessor.ALPHA, 0.5f).target(1).repeatYoyo(0, 0).setCallback(new TweenCallback() {
 
-							@Override
-							public void onEvent(int type, BaseTween<?> source) {
-								game.setScreen(MainMenuScreen.getInstance(game));
-							}
-						}).start(tween);
-				// game.setScreen(MainMenuScreen.getInstance(null));
+					@Override
+					public void onEvent(int type, BaseTween<?> source) {
+						game.setScreen(MainMenuScreen.getInstance(game));
+					}
+				}).start(tween);
+				//game.setScreen(MainMenuScreen.getInstance(null));
 
 			}
 		} else {
@@ -340,7 +346,7 @@ public class MapScreen implements Screen {
 			exitButtonMenuInActive.draw(batch);
 
 		}
-
+		
 	}
 
 	private void volumeButton() {
@@ -376,17 +382,15 @@ public class MapScreen implements Screen {
 		}
 
 	}
-
+	
 	@Override
 	public void resize(int width, int height) {
-		// oCamera.viewportHeight = height;
-		// oCamera.viewportWidth = width;
-		// oCamera.update();
-		// oCamera.position.set(player.getSprite().getX(), player.getSprite().getY(),
-		// 0);
-		// Test stuff; setToOrtho method above achieves the effect much better and
-		// cleaner.
-
+		//oCamera.viewportHeight = height;
+		//oCamera.viewportWidth = width;
+		//oCamera.update();
+		//oCamera.position.set(player.getSprite().getX(), player.getSprite().getY(), 0);
+		//Test stuff; setToOrtho method above achieves the effect much better and cleaner.
+		
 	}
 
 	@Override
@@ -394,11 +398,11 @@ public class MapScreen implements Screen {
 	}
 
 	@Override
-	public void resume() {
-	}
+	public void resume() {	}
 
 	@Override
-	public void hide() {
+	public void hide() 
+	{
 		dispose();
 	}
 
@@ -414,13 +418,15 @@ public class MapScreen implements Screen {
 		playButtonMenuInActive.getTexture().dispose();
 
 	}
-
-	public TiledMap getTileMap() {
+	
+	public TiledMap getTileMap()
+	{
 		return tilemap;
 	}
-
+	
 	public TiledModel getTiledModel() {
 		return model;
 	}
-
+	
 }
+
