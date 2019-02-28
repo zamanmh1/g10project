869f322a435da22842fe9f531bd5726a20a75f49
Shibu George <shibugeorge4@aol.com>
2019-02-28 00:49:06 +0000

    Commented and added changes to puzzle screen

--- GDX/core/src/com/mygdx/prisonescapegame/screens/PuzzleScreen.java
+++ GDX/core/src/com/mygdx/prisonescapegame/screens/PuzzleScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.mygdx.prisonescapegame.PrisonEscapeGame;
 
 import java.util.Random;
@@ -15,18 +16,25 @@ import java.util.Random;
  *
  */
 public class PuzzleScreen implements Screen {
-	
+
 	private PrisonEscapeGame game;
 	String imageLoc = "data/puzzles/";
 	private SlidingTile[][] tiles;
 	private SlidingTile empty;
-	boolean puzzleFinished = true;
+	boolean isPuzzleFinished = true;
+	private BitmapFont font;
 
+	/**
+	 * Creates a 3 x 3 puzzle tile with one tile black. Creates SlidingTile to get
+	 * the textures at each position. Random at each time PuzzleScreen is called so
+	 * the tiles are jumbled up.
+	 * 
+	 * @param game
+	 */
 	public PuzzleScreen(PrisonEscapeGame game) {
-		
 
 		this.game = game;
-
+		font = new BitmapFont(Gdx.files.internal("data/fonts/vision-bold-font.fnt"));
 		this.tiles = new SlidingTile[4][4];
 		for (int x = 1; x < 4; x++) {
 			for (int y = 1; y < 4; y++) {
@@ -42,6 +50,14 @@ public class PuzzleScreen implements Screen {
 		}
 	}
 
+	/**
+	 * When user clicks on the tile, it will swap from the empty tile.
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 */
 	private void swapTiles(int x1, int y1, int x2, int y2) {
 		SlidingTile temp = this.tiles[x1][y1];
 		this.tiles[x1][y1] = this.tiles[x2][y2];
@@ -52,44 +68,63 @@ public class PuzzleScreen implements Screen {
 	}
 
 	public void render(float delta) {
+	
 		this.game.getGameController().getSpriteBatch().begin();
-
-		this.puzzleFinished = true;
+		font.draw(game.getGameController().getSpriteBatch(), "Complete puzzle to continue...", PrisonEscapeGame.WIDTH / 2,
+				PrisonEscapeGame.HEIGHT / 2 + 200);
+		this.isPuzzleFinished = true;
 		for (int x = 1; x < 4; x++) {
 			for (int y = 1; y < 4; y++) {
 				float size = 128.0F;
-				float xLoc = (x - 1) * size;
+				float xLoc = (x - 1) * size + 600;
 				float yLoc = 4.0F * size - y * size;
-				int hover = 0;
 
 				float mouseX = Gdx.input.getX();
 				float mouseY = Gdx.graphics.getHeight() - Gdx.input.getY();
 				if ((mouseX > xLoc) && (mouseX < xLoc + size) && (mouseY > yLoc) && (mouseY < yLoc + size)) {
-					this.game.getGameController().getSpriteBatch().draw(this.tiles[x][y].getHoverTexture(), xLoc, yLoc, size, size);
+					this.game.getGameController().getSpriteBatch().draw(this.tiles[x][y].getPuzzleImageHover(), xLoc,
+							yLoc, size, size);
 					if (Gdx.input.isTouched()) {
 						moveTile(x, y);
 					}
 				} else {
-					this.game.getGameController().getSpriteBatch().draw(this.tiles[x][y].getTexture(), xLoc, yLoc, size, size);
+					this.game.getGameController().getSpriteBatch().draw(this.tiles[x][y].getPuzzleImage(), xLoc, yLoc,
+							size, size);
 				}
 				if (!this.tiles[x][y].inCorrectPosition(x, y)) {
-					this.puzzleFinished = false;
+					this.isPuzzleFinished = false;
 				}
 			}
 		}
-		
-		if (this.puzzleFinished) {
+
+		if (this.isPuzzleFinished) {
 			dispose();
 			this.game.setScreen(game.getGameController().getMapScreen());
 		}
 		this.game.getGameController().getSpriteBatch().end();
 	}
 
+	/**
+	 * Checks whether tile has been moved
+	 * 
+	 * @param r
+	 * @param c
+	 * @return true or false
+	 */
 	public boolean moveTile(int r, int c) {
 		return (checkEmpty(r, c, -1, 0)) || (checkEmpty(r, c, 1, 0)) || (checkEmpty(r, c, 0, -1))
 				|| (checkEmpty(r, c, 0, 1));
 	}
 
+	/**
+	 * Checks to see if its empty
+	 * 
+	 * @param x
+	 * @param y
+	 * @param xChange
+	 * @param yChange
+	 * @return true or false
+	 */
 	private boolean checkEmpty(int x, int y, int xChange, int yChange) {
 		int xNext = x + xChange;
 		int yNext = y + yChange;
@@ -100,6 +135,13 @@ public class PuzzleScreen implements Screen {
 		return false;
 	}
 
+	/**
+	 * Check whether the square is legal and can be used
+	 * 
+	 * @param r
+	 * @param c
+	 * @return true or false
+	 */
 	public boolean isLegalSquare(int r, int c) {
 		return (r >= 0) && (r < 4) && (c >= 0) && (c < 4);
 	}
--- GDX/core/src/com/mygdx/prisonescapegame/screens/SlidingTile.java
+++ GDX/core/src/com/mygdx/prisonescapegame/screens/SlidingTile.java
@@ -2,55 +2,90 @@ package com.mygdx.prisonescapegame.screens;
 
 import com.badlogic.gdx.graphics.Texture;
 
-public class SlidingTile
-{
-  Texture texture;
-  Texture hoverTexture;
-  int finishRow;
-  int finishCol;
-  int currentRow;
-  int currentCol;
-  boolean used = false;
-  String imageLoc = "data/puzzles/";
-  
-  public SlidingTile(int x, int y)
-  {
-    this.finishRow = x;
-    this.finishCol = y;
-    
-    this.texture = new Texture(this.imageLoc + x + y + ".jpg");
-    this.hoverTexture = new Texture(this.imageLoc + x + y + "_hover.jpg");
-  }
-  
-  public int getRow()
-  {
-    return this.currentRow;
-  }
-  
-  public int getCol()
-  {
-    return this.currentCol;
-  }
-  
-  public Texture getTexture()
-  {
-    return this.texture;
-  }
-  
-  public Texture getHoverTexture()
-  {
-    return this.hoverTexture;
-  }
-  
-  public void clearTexture()
-  {
-    this.texture = new Texture(this.imageLoc + "black.png");
-    this.hoverTexture = new Texture(this.imageLoc + "black.png");
-  }
-  
-  public boolean inCorrectPosition(int x, int y)
-  {
-    return (x == this.finishRow) && (y == this.finishCol);
-  }
-}
+/**
+ * 
+ * 
+ * @author Shibu George
+ *
+ */
+public class SlidingTile {
+	Texture puzzleImage;
+	Texture puzzleImageHover;
+	int finishRow;
+	int finishCol;
+	int currentRow;
+	int currentColumn;
+	boolean used = false;
+	String imageLoc = "data/puzzles/";
 
+	/**
+	 * Creates row of x and y and assigns texture to each position, both puzzleImage
+	 * and puzzleImageHover when the mouse is over the image
+	 * 
+	 * @param x position
+	 * @param y position
+	 */
+	public SlidingTile(int x, int y) {
+		this.finishRow = x;
+		this.finishCol = y;
+
+		this.puzzleImage = new Texture(this.imageLoc + x + y + ".jpg");
+		this.puzzleImageHover = new Texture(this.imageLoc + x + y + "_hover.jpg");
+	}
+
+	/**
+	 * Returns the current row 
+	 * 
+	 * @return currentRow
+	 */
+	public int getRow() {
+		return this.currentRow;
+	}
+
+	/**
+	 * Returns the current column 
+	 * 
+	 * @return currentColum
+	 */
+	public int getColumn() {
+		return this.currentColumn;
+	}
+
+	/**
+	 * Returns the puzzle image
+	 * 
+	 * @return puzzleImage
+	 */
+	public Texture getPuzzleImage() {
+		return this.puzzleImage;
+	}
+
+	/**
+	 * Returns the puzzle image hover
+	 * 
+	 * @return puzzleImageHover
+	 */
+	public Texture getPuzzleImageHover() {
+		return this.puzzleImageHover;
+	}
+
+	/**
+	 * Texture with the black background
+	 * 
+	 */
+	public void clearTexture() {
+		this.puzzleImage = new Texture(this.imageLoc + "black.png");
+		this.puzzleImageHover = new Texture(this.imageLoc + "black.png");
+	}
+
+	/**
+	 * Checks if its in the right position
+	 * 
+	 * @param x 
+	 * @param y
+	 * @return boolean true or false
+	 */
+	public boolean inCorrectPosition(int x, int y) {
+		return (x == this.finishRow) && (y == this.finishCol);
+	}
+}
