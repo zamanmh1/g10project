a581be26ffc0a39cb7e9bf95b3522c9ad9c5f1c6
Samuel Ward <wards4@aston.ac.uk>
2019-03-17 22:53:54 +0000

    Start of testing with errors

---
+++ GDX/core/src/com/mygdx/game/testing/TestTiledModel.java
@@ -0,0 +1,31 @@
+package com.mygdx.game.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.mygdx.game.model.TiledModel;
+
+public class TestTiledModel {
+
+	private TiledModel model;
+	private TiledMap tilemap;
+	
+	@Before
+	public void setUp() {
+		String map = "data/maps/cell.tmx";
+		//String path = Gdx.files.internal(map).path();
+		tilemap = new TmxMapLoader().load(map);
+		model = new TiledModel(tilemap);
+	}
+	
+	@Test
+	public void testModel_NotNull() {
+		
+	}
+
+}
