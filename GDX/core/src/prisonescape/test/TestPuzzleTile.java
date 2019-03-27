93e3d48a5b14c952f16b45b230b517bb9864e6c1
Shibu George <shibugeorge4@aol.com>
2019-03-27 18:12:28 +0000

    Javadoc of TestPuzzleTile

--- GDX/core/src/prisonescape/test/TestPuzzleTile.java
+++ GDX/core/src/prisonescape/test/TestPuzzleTile.java
@@ -7,19 +7,53 @@ import org.junit.Test;
 
 import prisonescape.game.screens.components.PuzzleTile;
 
-public class TestPuzzleTile{
+/**
+ * A class which test if the puzzle tiles are in the correct position
+ * 
+ * @author Shibu George
+ *
+ */
+public class TestPuzzleTile {
 
-	private PuzzleTile puzzleTile1;
+	/**
+	 * 2D array of the puzzle tiles
+	 * 
+	 */
+	private PuzzleTile[][] tiles;
+	
 
+	/**
+	 * Creating 4 x 4 row and creating PuzzleTile
+	 * 
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
-		puzzleTile1 = new PuzzleTile(1,1);
-		//puzzleTile1.setPuzzleImage("data/puzzles/puzzle1/11.png");
+		this.tiles = new PuzzleTile[4][4];
+		for (int x = 1; x < 4; x++) {
+			for (int y = 1; y < 4; y++) {
+
+				this.tiles[x][y] = new PuzzleTile(x, y);
+				
+				
+			}
+		}
+
 	}
 
+	/**
+	 * Testing whether each tile is in the right position.
+	 * 
+	 */
 	@Test
 	public void testInCorrectPosition() {
-		assertTrue(puzzleTile1.inCorrectPosition(1, 1));
+
+		for (int x = 1; x < 4; x++) {
+			for (int y = 1; y < 4; y++) {
+				assertTrue(this.tiles[x][y].inCorrectPosition(x, y));
+
+			}
+		}
 	}
 
 }
